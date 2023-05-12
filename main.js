353719b60057027e5c9dab3943e7f61dfd3af3ca
readyadam <133392811+readyadam@users.noreply.github.com>
2023-05-12 19:23:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+window.open('https://readyadam.github.io/Game/', '_blank');
+
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+  window.open('https://readyadam.github.io/Game/', '_blank');
+});
+
